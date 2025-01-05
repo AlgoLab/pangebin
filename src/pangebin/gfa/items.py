1632f9648295bf709e6ebaf22b58be50ea84ce3b
vepain <victorepain@disroot.org>
2025-01-05 14:29:35 +0100

    feat: :sparkles: Wrap gfapy API for link and segment

--- src/pangebin/gfa/items.py
+++ src/pangebin/gfa/items.py
@@ -1,6 +1,10 @@
 """GFA items module."""
 
+from __future__ import annotations
+
 from enum import StrEnum
+
+import gfapy  # type: ignore[import-untyped]
 
 
 class GFALineType(StrEnum):
@@ -37,3 +41,128 @@ class SkesaFixHeaderTagValue(StrEnum):
 
     YES = "Y"
     NO = "N"
+
+
+class SequenceTag(StrEnum):
+    """Sequence tags."""
+
+    LENGTH = "LN"
+    NORMALIZED_COVERAGE = "dp"
+
+
+class SequenceTagType(StrEnum):
+    """Sequence tag types."""
+
+    LENGTH = GFAFieldType.SIGNED_INT
+    NORMALIZED_COVERAGE = GFAFieldType.FLOAT
+
+
+class Orientation(StrEnum):
+    """Orientation of a fragment."""
+
+    FORWARD = "+"
+    REVERSE = "-"
+
+    @classmethod
+    def from_reverse_string(cls, reverse_string: str) -> Orientation:
+        """Get orientation from reverse string."""
+        match reverse_string:
+            case "+":
+                return cls.REVERSE
+            case "-":
+                return cls.FORWARD
+            case _:
+                _err_msg = f"Invalid reverse string: {reverse_string}"
+                raise ValueError(_err_msg)
+
+    def reverse(self) -> Orientation:
+        """Get reverse orientation."""
+        match self:
+            case Orientation.FORWARD:
+                return Orientation.REVERSE
+            case Orientation.REVERSE:
+                return Orientation.FORWARD
+
+
+class OrientedFragment:
+    """Oriented fragment."""
+
+    @classmethod
+    def from_left_dovetail_line(
+        cls,
+        link_line: gfapy.Line,
+        segment_name: str,
+    ) -> OrientedFragment:
+        """Get fragment from left dovetail line."""
+        if link_line.to_segment.name == segment_name:
+            return cls(link_line.from_segment.name, Orientation(link_line.from_orient))
+        return cls(
+            link_line.to_segment.name,
+            Orientation.from_reverse_string(link_line.to_orient),
+        )
+
+    @classmethod
+    def from_right_dovetail_line(
+        cls,
+        link_line: gfapy.Line,
+        segment_name: str,
+    ) -> OrientedFragment:
+        """Get fragment from right dovetail line."""
+        if link_line.from_segment.name == segment_name:
+            return cls(link_line.to_segment.name, Orientation(link_line.to_orient))
+        return cls(
+            link_line.from_segment.name,
+            Orientation.from_reverse_string(link_line.from_orient),
+        )
+
+    def __init__(self, identifier: str, orientation: Orientation) -> None:
+        """Initialize object."""
+        self.__identifier = identifier
+        self.__orientation = orientation
+
+    def identifier(self) -> str:
+        """Get identifier."""
+        return self.__identifier
+
+    def orientation(self) -> Orientation:
+        """Get orientation."""
+        return self.__orientation
+
+    def to_reverse(self) -> OrientedFragment:
+        """Get reverse fragment."""
+        return OrientedFragment(self.__identifier, self.__orientation.reverse())
+
+    def __str__(self) -> str:
+        """Get string representation."""
+        return f"{self.__identifier}\t{self.__orientation}"
+
+
+class Link:
+    """Link."""
+
+    def __init__(
+        self,
+        predecessor: OrientedFragment,
+        successor: OrientedFragment,
+    ) -> None:
+        """Initialize object."""
+        self.__predecessor = predecessor
+        self.__successor = successor
+
+    def predecessor(self) -> OrientedFragment:
+        """Get predecessor."""
+        return self.__predecessor
+
+    def successor(self) -> OrientedFragment:
+        """Get successor."""
+        return self.__successor
+
+    def to_reverse(self) -> Link:
+        """Get reverse link."""
+        return Link(self.__successor.to_reverse(), self.__predecessor.to_reverse())
+
+    def to_gfa_link_line(self) -> gfapy.Line:
+        """Get GFA link line."""
+        return gfapy.Line(
+            f"L\t{self.__predecessor}\t{self.__successor}\t0M",
+        )
--- src/pangebin/gfa/iter.py
+++ src/pangebin/gfa/iter.py
@@ -7,6 +7,8 @@ from typing import TYPE_CHECKING
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 
+from pangebin.gfa.items import Orientation, OrientedFragment
+
 if TYPE_CHECKING:
     from collections.abc import Iterator
 
@@ -66,4 +68,64 @@ def iter_gfa_to_fasta(
             id=seg.name,
             name=seg.name,
             description=f"{seg.name}.GFA {__gfa_segment_attribute_to_string(seg, sep=sep)}",  # noqa: E501
+        )
+
+
+def left_dovetails(
+    graph: gfapy.Gfa,
+    fragment: OrientedFragment,
+) -> Iterator[OrientedFragment]:
+    """Iterate over left dovetails.
+
+    Parameters
+    ----------
+    graph: gfapy.Gfa
+        GFA graph
+    fragment: OrientedFragment
+        fragment
+
+    Yields
+    ------
+    OrientedFragment
+        left dovetail
+
+    """
+    link_line: str
+    for link_line in graph.segments[fragment.identifier()].dovetails_L:
+        tab_link_line = link_line.split("\t")
+        if tab_link_line[3] == fragment.identifier():
+            yield OrientedFragment(tab_link_line[1], Orientation(tab_link_line[2]))
+        yield OrientedFragment(
+            tab_link_line[3],
+            Orientation.from_reverse_string(tab_link_line[4]),
+        )
+
+
+def right_dovetails(
+    graph: gfapy.Gfa,
+    fragment: OrientedFragment,
+) -> Iterator[OrientedFragment]:
+    """Iterate over right dovetails.
+
+    Parameters
+    ----------
+    graph: gfapy.Gfa
+        GFA graph
+    fragment: OrientedFragment
+        fragment
+
+    Yields
+    ------
+    OrientedFragment
+        right dovetail
+
+    """
+    link_line: str
+    for link_line in graph.segments[fragment.identifier()].dovetails_R:
+        tab_link_line = link_line.split("\t")
+        if tab_link_line[1] == fragment.identifier():
+            yield OrientedFragment(tab_link_line[3], Orientation(tab_link_line[4]))
+        yield OrientedFragment(
+            tab_link_line[1],
+            Orientation.from_reverse_string(tab_link_line[2]),
         )
