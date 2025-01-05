93acbf83a14c37088b9686adc084e151f9032cf1
vepain <victorepain@disroot.org>
2025-01-05 14:31:25 +0100

    feat: :sparkles: Fix temporary file gz

--- src/pangebin/gfa/ops.py
+++ src/pangebin/gfa/ops.py
@@ -4,14 +4,21 @@ from __future__ import annotations
 
 import datetime
 import logging
+import shutil
 from typing import TYPE_CHECKING
 
+import gfapy  # type: ignore[import-untyped]
+from gfapy.line.segment import Segment as GfaSegment  # type: ignore[import-untyped]
+
 import pangebin.input_output as io
+from pangebin.assembler import ContigPrefix
 from pangebin.gfa.items import (
     SKESA_FIX_HEADER_TAG,
     SKESA_FIX_HEADER_TAG_TYPE,
     GFAFieldType,
     GFALineType,
+    SequenceTag,
+    SequenceTagType,
     SkesaFixHeaderTagValue,
 )
 
@@ -71,8 +78,7 @@ def fix_skesa_gfa(
 
     if out_gfa_path is None:
         out_gfa_path = in_gfa_path.parent / (
-            f"{datetime.datetime.now(tz=datetime.UTC).isoformat()}"
-            f"_{in_gfa_path.name}"
+            f"{datetime.datetime.now(tz=datetime.UTC).isoformat()}.gfa"
         )
         _LOGGER.debug("Temporary output file: %s", out_gfa_path)
     elif in_gfa_path == out_gfa_path:
@@ -109,5 +115,90 @@ def fix_skesa_gfa(
                 f_out.write(line)
 
     if replace_file:
+        _LOGGER.debug(
+            "Replacing temporary output file %s with input file %s.",
+            out_gfa_path,
+            in_gfa_path,
+        )
         in_gfa_path.unlink()
-        out_gfa_path.replace(in_gfa_path)
+        with (
+            io.open_file_read(out_gfa_path) as f_in,
+            io.open_file_write(in_gfa_path) as f_out,
+        ):
+            shutil.copyfileobj(f_in, f_out)
+        out_gfa_path.unlink()
+
+
+def rename_contigs(
+    graph: gfapy.Gfa,
+    segment_name_prefix: ContigPrefix,
+) -> None:
+    """Rename contigs in a GFA graph.
+
+    Parameters
+    ----------
+    graph : gfapy.Gfa
+        GFA graph
+    segment_name_prefix : ContigPrefix
+        Prefix for contig names
+
+    Warnings
+    --------
+    This function mutates the GFA graph
+
+    """
+    _old_level = graph.vlevel
+    graph.vlevel = 3
+
+    seg: GfaSegment
+    for counter, seg in enumerate(graph.segments):
+        if seg.LN is not None:
+            if seg.LN == 0:
+                seg.sequence = gfapy.Placeholder()
+                seg.LN = 0
+        elif len(seg.sequence) == 0:
+            seg.sequence = gfapy.Placeholder()
+            seg.LN = 0
+        seg.name = f"{segment_name_prefix}{counter + 1}"
+
+    graph.vlevel = _old_level
+
+
+def convert_kc_to_dp(
+    graph: gfapy.Gfa,
+) -> None:
+    """Convert k-mer coverage to normalized coverage.
+
+    Parameters
+    ----------
+    graph : gfapy.Gfa
+        GFA graph
+
+    Warnings
+    --------
+    This function mutates the GFA graph
+
+    """
+    total_coverage = 0
+    total_length = 0
+
+    _old_level = graph.vlevel
+    graph.vlevel = 3
+
+    seg: GfaSegment
+    for seg in graph.segments:
+        total_coverage += seg.KC
+        if seg.LN is None:
+            seg.set_datatype(SequenceTag.LENGTH, SequenceTagType.LENGTH)
+            seg.LN = len(seg.sequence)
+        total_length += seg.LN
+
+    for seg in graph.segments:
+        seg.set_datatype(
+            SequenceTag.NORMALIZED_COVERAGE,
+            SequenceTagType.NORMALIZED_COVERAGE,
+        )
+        seg.dp = float((seg.KC * total_length) / (seg.LN * total_coverage))
+        seg.KC = None
+
+    graph.vlevel = _old_level
