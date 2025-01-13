e604a7c8095bd3b1bfcc9b22dc9a1a2a14001122
vepain <victorepain@disroot.org>
2025-01-13 10:40:09 +0100

    refactor: :recycle: Move ContigPrefix to NamePrefix

---
+++ src/pangebin/gfa/assembler/segment.py
@@ -0,0 +1,23 @@
+"""GFA segment API wrapper for assembler."""
+
+from __future__ import annotations
+
+from enum import StrEnum
+
+from pangebin import assembler
+
+
+class NamePrefix(StrEnum):
+    """Name prefixes for assemblers."""
+
+    UNICYCLER = "uni"
+    SKESA = "ske"
+
+    @classmethod
+    def from_assembler(cls, assembler_id: assembler.Item) -> NamePrefix:
+        """Get prefix from assembler."""
+        match assembler_id:
+            case assembler.Item.UNICYCLER:
+                return cls.UNICYCLER
+            case assembler.Item.SKESA:
+                return cls.SKESA
