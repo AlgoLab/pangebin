625978a2c5148126542cf3ede8ab3266e0c1a311
vepain <victorepain@disroot.org>
2025-02-19 23:19:11 +0100

    feat:

--- src/pangebin/logging.py
+++ src/pangebin/logging.py
@@ -3,14 +3,20 @@
 from __future__ import annotations
 
 import logging
-import sys
 
 import typer
+from rich.console import Console
+from rich.logging import RichHandler
 
 __ROOT_APP_NAME = "pangebin"
 
+_LOGFORMAT = "%(asctime)s %(name)s [%(levelname)s] %(message)s"
+_LOGFORMAT_RICH = "%(message)s"
+
 _LOGGER = logging.getLogger(__ROOT_APP_NAME)
 _LOGGER.setLevel(logging.DEBUG)
+
+CONSOLE = Console()
 
 OPT_DEBUG = typer.Option(
     help="Debug mode",
@@ -23,16 +29,14 @@ def format_logger(
     """Format logger."""
     _LOGGER.handlers.clear()
     _LOGGER.filters.clear()
-    handler = logging.StreamHandler(sys.stdout)
+    rich_handler = RichHandler(console=CONSOLE)
     if debug:
-        handler.setLevel(logging.DEBUG)
+        rich_handler.setLevel(logging.DEBUG)
     else:
-        handler.setLevel(logging.INFO)
-    formatter = logging.Formatter(
-        "%(asctime)s %(name)s [%(levelname)s] %(message)s",
-    )
-    handler.setFormatter(formatter)
-    _LOGGER.addHandler(handler)
+        rich_handler.setLevel(logging.INFO)
+    rich_formatter = logging.Formatter(_LOGFORMAT_RICH, datefmt="%Y-%m-%dT%H:%M:%S%z")
+    rich_handler.setFormatter(rich_formatter)
+    _LOGGER.addHandler(rich_handler)
     log_filter = logging.Filter(__ROOT_APP_NAME)
     _LOGGER.addFilter(log_filter)
 
