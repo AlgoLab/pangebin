c92bcbb3b10890c5f781af6fd4b643478335446f
vepain <victorepain@disroot.org>
2025-02-21 18:00:09 +0100

    feat: :sparkles: Unicycler API wrapper

---
+++ src/pangebin/assembly/create.py
@@ -0,0 +1,26 @@
+"""Assemble reads."""
+
+from pathlib import Path
+
+from pangebin import subprocess_lib
+
+UNICYCLER_CMD_STR = "unicycler"
+
+
+def unicycler_paired_end_reads(
+    reads_1_fastq: Path,
+    reads_2_fastq: Path,
+    out_directory: Path,
+) -> None:
+    """Assemble paired short reads with Unicycler."""
+    cmd_path = subprocess_lib.command_path(UNICYCLER_CMD_STR)
+    cli_line = [
+        cmd_path,
+        "-1",
+        reads_1_fastq,
+        "-2",
+        reads_2_fastq,
+        "-o",
+        out_directory,
+    ]
+    subprocess_lib.run_cmd(cli_line, UNICYCLER_CMD_STR)
