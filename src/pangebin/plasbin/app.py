3df4421c81cb6064481a4c13a61b558501a60d31
vepain <victorepain@disroot.org>
2025-03-19 16:32:17 -0700

    feat: :sparkles: Plasbin typer interface

--- src/pangebin/plasbin/app.py
+++ src/pangebin/plasbin/app.py
@@ -1,7 +1,5 @@
 """Plasbin application module."""
 
-# TODO pangebin app
-
 # Due to typer usage:
 # ruff: noqa: TC001, TC003, UP007, FBT001, FBT002, PLR0913
 
@@ -13,92 +11,133 @@ from typing import Annotated
 
 import typer
 
+import pangebin.gc_content.input_output as gc_io
+import pangebin.gfa.input_output as gfa_io
 import pangebin.logging as common_log
-from pangebin.gc_content import create, items
+import pangebin.plasbin.bins.input_output as bin_io
+import pangebin.plasbin.config as pb_cfg
+import pangebin.plasbin.create as pb_create
+import pangebin.plasbin.input_output as pb_io
+import pangebin.plasmidness as plm
+import pangebin.seed.input_output as seed_io
 
 _LOGGER = logging.getLogger(__name__)
 
 APP = typer.Typer(rich_markup_mode="rich")
 
 
-DEFAULT_GC_CONTENT_INTERVAL_FILE = (
-    Path(__file__).parent / "default_gc_content_intervals.txt"
-)
+class Arguments:
+    """PangeBin-flow arguments."""
 
-
-class GFAScoresArguments:
-    """GFA GC content arguments."""
-
-    GFA_FILE = typer.Argument(
-        help="GFA assembly graph file",
+    PANASSEMBLY_GFA = typer.Argument(
+        help="Pan-assembly GFA file",
     )
 
-    INTERVALS_AND_SCORES_YAML_FILE = typer.Argument(
-        help="Output YAML file with intervals and scores",
+    GC_SCORES_TSV = typer.Argument(
+        help="TSV file with the sequences and their GC scores",
+    )
+
+    PLASMIDNESS_TSV = typer.Argument(
+        help="TSV file with the sequences and their plasmidness scores",
+    )
+
+    SEEDS_TSV = typer.Argument(
+        help="TSV file with the seeds",
     )
 
 
-class ScoresOptions:
-    """GC content options."""
+class MILPOptions:
+    """MILP options."""
 
-    _RICH_HELP_PANEL = "GFA GC content options"
+    _RICH_HELP_PANEL = "MILP options"
 
-    GC_CONTENT_INTERVAL_FILE = typer.Option(
-        help="GC content intervals file",
+    MCF_COEFFICIENT = typer.Option(
+        help="MCF coefficient",
         rich_help_panel=_RICH_HELP_PANEL,
     )
 
-    PSEUDO_COUNT = typer.Option(
-        help="Pseudo count for GC probabilities",
+    MGC_COEFFICIENT = typer.Option(
+        help="MGC coefficient",
+        rich_help_panel=_RICH_HELP_PANEL,
+    )
+
+    CONFIG_FILE = typer.Option(
+        "--milp-cfg",
+        help="The configuration file path",
+        rich_help_panel=_RICH_HELP_PANEL,
+    )
+
+
+class IOOptions:
+    """Input-output options."""
+
+    _RICH_HELP_PANEL = "Input/Output options"
+
+    OUTPUT_DIR = typer.Option(
+        help="Output directory",
         rich_help_panel=_RICH_HELP_PANEL,
     )
 
 
 @APP.command()
-def from_gfa(
-    gfa_file: Annotated[Path, GFAScoresArguments.GFA_FILE],
-    intervals_and_scores_yaml_file: Annotated[
-        Path,
-        GFAScoresArguments.INTERVALS_AND_SCORES_YAML_FILE,
-    ],
-    gc_content_interval_file: Annotated[
-        Path | None,
-        ScoresOptions.GC_CONTENT_INTERVAL_FILE,
-    ] = None,
-    pseudo_count: Annotated[
-        int,
-        ScoresOptions.PSEUDO_COUNT,
-    ] = create.DEFAULT_PSEUDO_COUNT,
+def plasbin(
+    panassembly_gfa: Annotated[Path, Arguments.PANASSEMBLY_GFA],
+    gc_scores_tsv: Annotated[Path, Arguments.GC_SCORES_TSV],
+    plasmidness_tsv: Annotated[Path, Arguments.PLASMIDNESS_TSV],
+    seeds_tsv: Annotated[Path, Arguments.SEEDS_TSV],
+    # MILP options
+    mcf_coefficient: Annotated[
+        float,
+        MILPOptions.MCF_COEFFICIENT,
+    ] = pb_cfg.Config.DEFAULT_GAMMA_MCF,
+    mgc_coefficient: Annotated[
+        float,
+        MILPOptions.MGC_COEFFICIENT,
+    ] = pb_cfg.Config.DEFAULT_GAMMA_MGC,
+    milp_cfg_yaml: Annotated[Path | None, MILPOptions.CONFIG_FILE] = None,
+    # IO options
+    outdir: Annotated[Path, IOOptions.OUTPUT_DIR] = pb_io.Config.DEFAULT_OUTPUT_DIR,
     debug: Annotated[bool, common_log.OPT_DEBUG] = False,
 ) -> None:
-    """Compute GC scores from GFA assembly graph."""
-    common_log.init_logger(
-        _LOGGER,
-        "Computing GC scores from GFA assembly graph.",
-        debug,
-    )
+    """PlasBin application."""
+    common_log.init_logger(_LOGGER, "Running PlasBin.", debug)
+    outdir.mkdir(parents=True, exist_ok=True)
 
-    # REFACTOR: check file exist in class methods
-    if not gfa_file.exists():
-        _LOGGER.error("Input GFA file does not exist: %s", gfa_file)
-        raise typer.Exit(1)
+    io_manager = pb_io.Manager(pb_io.Config(output_directory=outdir))
 
-    if gc_content_interval_file is None:
-        gc_content_interval_file = DEFAULT_GC_CONTENT_INTERVAL_FILE
-
-    if not gc_content_interval_file.exists():
-        _LOGGER.error(
-            "GC content interval file does not exist: %s",
-            gc_content_interval_file,
+    milp_config = (
+        pb_cfg.Config.from_yaml(milp_cfg_yaml)
+        if milp_cfg_yaml is not None
+        else pb_cfg.Config(
+            gamma_mcf=mcf_coefficient,
+            gamma_mgc=mgc_coefficient,
         )
-        raise typer.Exit(1)
-
-    gc_content_intervals = items.Intervals.from_file(gc_content_interval_file)
-
-    intervals_and_scores = create.gfa_file_to_gc_scores(
-        gfa_file,
-        gc_content_intervals,
-        pseudo_count=pseudo_count,
     )
 
-    intervals_and_scores.to_yaml(intervals_and_scores_yaml_file)
+    with gc_io.Reader.open(gc_scores_tsv) as gc_scores_fin:
+        intervals = gc_scores_fin.intervals()
+        gc_scores = list(gc_scores_fin)
+    with plm.Reader.open(plasmidness_tsv) as plasmidness_fin:
+        plasmidness = list(plasmidness_fin)
+    with seed_io.Reader.open(seeds_tsv) as seeds_fin:
+        seeds = list(seeds_fin)
+
+    for k, (bin_stats, seq_normcovs) in enumerate(
+        pb_create.plasbin(
+            gfa_io.from_file(panassembly_gfa),
+            intervals,
+            gc_scores,
+            plasmidness,
+            seeds,
+            milp_config,
+            io_manager.config().output_directory(),
+        ),
+    ):
+        io_manager.bin_outdir(k).mkdir(parents=True, exist_ok=True)
+        bin_stats.to_yaml(io_manager.bin_stats_path(k))
+        with bin_io.Writer.open(io_manager.bin_seq_normcov_path(k)) as fout:
+            for seq_normcov in seq_normcovs:
+                fout.write_sequence_normcov(
+                    seq_normcov.identifier(),
+                    seq_normcov.normalized_coverage(),
+                )
