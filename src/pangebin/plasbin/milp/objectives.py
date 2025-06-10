8ab09f0b64509ae514dca07bd721135e1387c61a
vepain <victorepain@disroot.org>
2025-06-10 14:13:57 -0700

    feat: :sparkles: New objective function domain function

--- src/pangebin/plasbin/milp/objectives.py
+++ src/pangebin/plasbin/milp/objectives.py
@@ -3,8 +3,10 @@
 from collections.abc import Callable, Iterable, Iterator
 from enum import StrEnum
 
-import pangebin.gfa.segment as gfa_segment
 import pangebin.plasbin.network as net
+
+# REFACTOR to different class
+# REFACTOR move to cli the choice of a final class
 
 
 class ObjectiveFunctionDomain(StrEnum):
@@ -17,6 +19,8 @@ class ObjectiveFunctionDomain(StrEnum):
         """Get objective function domain function."""
         match self:
             case ObjectiveFunctionDomain.ALL:
+                # XXX PAER TESTS
+                # return all_but_list
                 return net.Network.fragment_ids
             case ObjectiveFunctionDomain.SEEDS:
                 return net.Network.seeds
@@ -24,17 +28,11 @@ class ObjectiveFunctionDomain(StrEnum):
                 raise ValueError
 
 
-def max_frag_length(
-    network: net.Network,
-    frag_set_fn: Callable[[net.Network], Iterable[str]],
-) -> int:
-    """Get maximum fragment length."""
-    return max(
-        gfa_segment.length(network.gfa_graph().segment(frag_id))
-        for frag_id in frag_set_fn(network)
-    )
+def all_but_list(network: net.Network, refused: Iterable[str]) -> Iterator[str]:
+    """Get all but list."""
+    return (frag_id for frag_id in network.fragment_ids() if frag_id not in refused)
 
 
 def zeta_i(network: net.Network, frag_id: str, max_frag_length: int) -> float:
     """Get zeta_i coefficient."""
-    return gfa_segment.length(network.gfa_graph().segment(frag_id)) / max_frag_length
+    return net.length(network, frag_id) / max_frag_length
