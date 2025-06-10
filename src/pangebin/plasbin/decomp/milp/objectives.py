a537a73bac3d14ab08b2d64459d1e32e75d7a597
vepain <victorepain@disroot.org>
2025-06-10 14:15:21 -0700

    refactor: :recycle: Rename methods

--- src/pangebin/plasbin/decomp/milp/objectives.py
+++ src/pangebin/plasbin/decomp/milp/objectives.py
@@ -18,13 +18,13 @@ def coverage_score(
     """Get linear expression for coverage score."""
     # DOCU MCF: Tests on coverage score
     frag_set_fn = pb_lp_obj.ObjectiveFunctionDomain.to_fn(obj_fun_domain)
-    max_frag_length = pb_lp_obj.max_frag_length(network, frag_set_fn)
+    max_frag_length = net.max_frag_length(network, frag_set_fn)
     return gurobipy.quicksum(
         pb_lp_obj.zeta_i(network, frag_id, max_frag_length)
         * (
             flow_vars.incoming_forward_reverse(network, frag_id)
             - (
-                network.coverage(frag_id) * frag_vars.x(frag_id)
+                network.coverage(frag_id) * frag_vars.frag(frag_id)
                 - flow_vars.incoming_forward_reverse(network, frag_id)
             )
         )
@@ -81,12 +81,12 @@ def plasmidness_score(
 ) -> gurobipy.LinExpr:
     """Get linear expression for plasmidness score."""
     frag_set_fn = pb_lp_obj.ObjectiveFunctionDomain.to_fn(obj_fun_domain)
-    max_frag_length = pb_lp_obj.max_frag_length(network, frag_set_fn)
+    max_frag_length = net.max_frag_length(network, frag_set_fn)
     # DOCU MPS: coeff in obj
     return gurobipy.quicksum(
         pb_lp_obj.zeta_i(network, frag_id, max_frag_length)
         * network.plasmidness(frag_id)
-        * frag_vars.x(frag_id)
+        * frag_vars.frag(frag_id)
         for frag_id in frag_set_fn(network)
     )
 
