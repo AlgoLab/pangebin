d783101154a545b668a4f29335b16bd72d2e449a
vepain <victorepain@disroot.org>
2025-06-10 14:10:04 -0700

    refactor: :recycle: Move function to gfa ops module

--- src/pangebin/std_asm_graph/ops.py
+++ src/pangebin/std_asm_graph/ops.py
@@ -1,17 +1,12 @@
 """Create standardized files."""
 
-import contextlib
 import logging
-from itertools import product
 
 import gfapy  # type: ignore[import-untyped]
 
 import pangebin.assembly.items as asm_items
 import pangebin.gfa.ops as gfa_ops
-import pangebin.gfa.segment as gfa_segment
 from pangebin.gfa.assembler import segment as gfa_asm_segment
-from pangebin.gfa.link import Link
-from pangebin.gfa.segment import OrientedFragment
 from pangebin.std_asm_graph.config import Config
 
 _LOGGER = logging.getLogger(__name__)
@@ -56,62 +51,8 @@ def standardize_assembly_graph(
     if assembler_id == asm_items.Identifier.SKESA:
         gfa_ops.convert_kmer_coverage_to_normalized_coverage(graph)
 
-    transform_small_contigs_into_links(graph, config.min_contig_length())
+    gfa_ops.transform_small_contigs_into_links(graph, config.min_contig_length())
 
     gfa_ops.set_standardized_header_tag(graph)
 
     # TODO get map between original and new segment names
-
-
-def transform_small_contigs_into_links(
-    gfa: gfapy.Gfa,
-    min_contig_length: int,
-) -> None:
-    """Remove small contigs and transform them into links.
-
-    Parameters
-    ----------
-    gfa : gfapy.Gfa
-        GFA graph
-    min_contig_length : int
-        Minimum contig length
-
-    Warnings
-    --------
-    This function mutates the GFA graph
-
-    """
-    for segment in gfa.segments:
-        if gfa_segment.length(segment) <= min_contig_length:
-            left_edge_lines = list(segment.dovetails_L)
-            right_edge_lines = list(segment.dovetails_R)
-            predecessors: list[OrientedFragment] = []
-            successors: list[OrientedFragment] = []
-
-            for left_link_line in left_edge_lines:
-                pred = OrientedFragment.from_left_dovetail_line(
-                    left_link_line,
-                    segment.name,
-                )
-                if pred.identifier() != segment.name:
-                    predecessors.append(pred)
-                    gfa.rm(left_link_line)
-
-            for right_link_line in right_edge_lines:
-                succ = OrientedFragment.from_right_dovetail_line(
-                    right_link_line,
-                    segment.name,
-                )
-                if succ.identifier() != segment.name:
-                    successors.append(succ)
-                    gfa.rm(right_link_line)
-
-            gfa.rm(segment)
-            gfa.validate()
-            for link in (
-                Link(pred, succ) for pred, succ in product(predecessors, successors)
-            ):
-                with contextlib.suppress(gfapy.NotUniqueError):
-                    # XXX the exception should not happen
-                    gfa.add_line(link.to_gfa_line())
-            gfa.validate()
